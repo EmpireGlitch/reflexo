19a8872b993be2249f2597dbae4fb9d80a8f9728
EmpireGlitch <empireglitch@gmail.com>
2017-05-25 15:03:47 +0300

    video frame update

--- js/video.js
+++ js/video.js
@@ -2,6 +2,7 @@ function showVideo(code){
     // Add iframe to #video-wrap
     var html = '<iframe width="750" height="550" src="https://www.youtube.com/embed/' + code + '"></iframe>'
     $('#video-wrap').append(html);
+    $('#video-wrap').show();
     showOverlay();
         
 }
